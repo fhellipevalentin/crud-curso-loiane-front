48b03fe175b77151ae47deed2ac81d98fa481880
Fhellipe Valentin <fhellipereis@outlook.com>
2023-03-28 14:48:57 -0300

    interatividade dos botões da inscricao de curso

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,7 +1,9 @@
-import { CoursesService } from './../services/courses.service';
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -11,7 +13,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class CourseFormComponent {
 
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder, private service: CoursesService, private _snackBar: MatSnackBar) {
+  constructor(private formBuilder: FormBuilder, private service: CoursesService, private _snackBar: MatSnackBar, private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -20,11 +22,17 @@ export class CourseFormComponent {
 
   onSubmit() {
     this.service.save(this.form.value)
-    .subscribe(data => console.log(data), error => this.onError())
+    .subscribe(data => this.onSuccess(), error => this.onError())
+
   }
 
   onCancel() {
+    this.location.back();
+  }
 
+  private onSuccess() {
+    this._snackBar.open("Curso salvo com sucesso!", '', { duration: 3000 });
+    this.onCancel();
   }
 
   private onError() {
