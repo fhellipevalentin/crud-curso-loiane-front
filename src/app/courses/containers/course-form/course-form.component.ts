820384efc0b0a84d9ede4b44ae376b2e0ffcbf4e
Fhellipe Valentin <fhellipereis@outlook.com>
2023-03-31 17:03:27 -0300

    atualizar curso: populando o formulario

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -2,8 +2,10 @@ import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 
 import { CoursesService } from '../../services/courses.service';
+import { Course } from '../../model/course';
 
 @Component({
   selector: 'app-course-form',
@@ -13,15 +15,26 @@ import { CoursesService } from '../../services/courses.service';
 export class CourseFormComponent implements OnInit{
 
   form = this.formBuilder.group({
+    _id: [''],
     name: [''],
     category: ['']
   });
-  constructor(private formBuilder: NonNullableFormBuilder, private service: CoursesService, private _snackBar: MatSnackBar, private location: Location) {
+  constructor(private formBuilder: NonNullableFormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar,
+    private location: Location,
+    private route: ActivatedRoute) {
     // this.form =
   }
 
   ngOnInit(): void {
   // this.form.value.name = 'Java';
+  const course: Course = this.route.snapshot.data['course'];
+  this.form.setValue({
+    _id: course._id,
+    name: course.name,
+    category: course.category
+  })
   }
 
   onSubmit() {
