5c90dfdd05aec5f90eb582ea988cecce7656c991
Fhellipe Valentin <fhellipereis@outlook.com>
2023-04-03 18:05:11 -0300

    remover curso

--- src/app/courses/components/courses-list/courses-list.component.ts
+++ src/app/courses/components/courses-list/courses-list.component.ts
@@ -12,6 +12,7 @@ export class CoursesListComponent {
 @Input() courses: Course[] = [];
 @Output() add = new EventEmitter(false);
 @Output() edit = new EventEmitter(false);
+@Output() remove = new EventEmitter(false);
 
 readonly displayedColumns = ['name', 'category', 'actions'];
 
@@ -27,4 +28,9 @@ onAdd() {
 onEdit(course: Course) {
   this.edit.emit(course);
 }
+
+onDelete(course: Course) {
+  this.remove.emit(course);
+}
+
 }
--- src/app/courses/containers/courses/courses.component.ts
+++ src/app/courses/containers/courses/courses.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
 import { catchError, Observable, of, pipe } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-courses',
@@ -13,23 +14,27 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class CoursesComponent implements OnInit{
 
-  courses$: Observable <Course[]>;
+  courses$: Observable <Course[]> | null = null;
   displayedColumns = ['name', 'category', 'actions'];
   // courseService: CoursesService
 
-  constructor(private courseService: CoursesService, public dialog: MatDialog, private router: Router, private route: ActivatedRoute) {
+  constructor(private courseService: CoursesService, public dialog: MatDialog, private router: Router, private route: ActivatedRoute, private snackbar: MatSnackBar) {
     // this.courses = [];
     // this.courseService = new CoursesService();
+    this.refresh()
+  }
+  ngOnInit(): void {
+    // this.courses = this.courseService.list();
+  }
+
+  refresh() {
     this.courses$ = this.courseService.list()
     .pipe (
-      catchError( error => {
+      catchError( () => {
         this.onError('Erro ao carregar cursos.')
         return of([]); // retorna um observable de array vazio
       })
     )
-  }
-  ngOnInit(): void {
-    // this.courses = this.courseService.list();
   }
 
   onError(errorMsg: string) {
@@ -45,4 +50,18 @@ export class CoursesComponent implements OnInit{
   onEdit(course: Course) {
     this.router.navigate(['edit', course._id], {relativeTo: this.route});
   }
+
+  onRemove(course: Course) {
+    this.courseService.remove(course._id).subscribe(() => {
+      this.refresh()
+      this.snackbar.open('Curso removido com sucesso!', 'X', {
+        duration: 5000,
+        verticalPosition: 'top',
+        horizontalPosition: 'center'
+      })
+    },
+    error => this.onError('Erro ao tentar remover curso.')
+    );
+  }
+
 }
