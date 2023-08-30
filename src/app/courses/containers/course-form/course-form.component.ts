4c37e5ca8cff526e7a83d304cdf7af025c14a462
fhellipevalentin <fhellipereis@outlook.com>
2023-08-30 17:31:56 -0300

    FormArray no HTML

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder, UntypedFormArray, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
@@ -29,31 +29,34 @@ export class CourseFormComponent implements OnInit{
   const course: Course = this.route.snapshot.data['course'];
   this.form = this.formBuilder.group({
     _id: [course._id],
-    name: [course.name, [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
-    category: [course.category, Validators.required],
+    name: [course.name, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+    category: [course.category, [Validators.required]],
     lessons: this.formBuilder.array(this.retrieveLessons(course))
-  })
-  console.log(this.form)
-  console.log(this.form.value)
+  });
+  console.log(this.form);
+  console.log(this.form.value);
   }
 
   private retrieveLessons(course: Course) {
     const lessons = [];
     if (course?.lessons) {
-      course.lessons.forEach(lesson => lessons.push(this.createLesson(lesson)))
+      course.lessons.forEach(lesson => lessons.push(this.createLesson(lesson)));
     } else {
       lessons.push(this.createLesson());
     }
     return lessons;
   }
 
-  private createLesson(lesson: Lesson = {_id: '', name: '', youtubeUrl: ''}) {
+  private createLesson(lesson: Lesson = { id: '', name: '', youtubeUrl: '' }) {
     return this.formBuilder.group({
-      _id: [lesson._id],
+      id: [lesson.id],
       name: [lesson.name],
       youtubeUrl: [lesson.youtubeUrl]
-    }
-    )
+    });
+  }
+
+  getLessonsFormArray() {
+    return (<UntypedFormArray>this.form.get('lessons')).controls;
   }
 
   onSubmit() {
